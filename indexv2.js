3ead75f0eb790e45cb038b317305143214e82cbf
Facu <facuovalles@gmail.com>
2023-06-16 12:49:36 -0300

    Validacion register completa

--- indexv2.js
+++ indexv2.js
@@ -1,37 +1,33 @@
+//Método validación de dominión nuevo
+var formatoEmailvalido = false
+var pruebaFormato  = dominios.some(function(elemento) {  
+    formatoEmailvalido =  elemento.test(formato2);   
+    return formatoEmailvalido
+});
 
-
-var prueba = 'facuovalles@gmail.com'
-var prueba2 = 'facuovalles@gmail.gov'
-var prueba3 = 'holatodobien.ar'
-
-var regexCom = /\.com$/;
-var regexOrg = /\.org$/;
-var regexNet = /\.net$/;
-var regexEdu = /\.edu$/;
-var regexGov = /\.gov$/;
-var regexMil = /\.mil$/;
-var valCorreo = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
-
-const dominios = [regexCom,regexOrg,regexNet,
-regexEdu,regexGov,regexMil] 
-
-function validarFormatoEmail (dominio) {
-  return valCorreo.test(dominio)
+if (formatoEmailvalido) {
+    console.log(formato)
+} 
+else{
+    console.log(formato)
+    alert('Dominio nó valido')
 }
 
-function validarDominioEmail (dominio, lista) {
-  for (var i = 0; i < lista.length; i++) {
-    if (lista[i].test(dominio))  {
-      console.log("El dominio es válido: " + dominio);
-      break; // Terminar el bucle si se encuentra una coincidencia
-    } 
-    else {
-      console.log("El dominio no es válido: " + dominio);
-      return false;
-    }  
-  }  
-}
 
-var usuario = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ // EMAIL VALIDO
-var nombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/ // Letras y espacios, pueden llevar acentos.
-var password = /^.{7,20}$/ // 7 a 20 digitos.
+
+var formato = "example@example.com";
+
+var pruebaFormato = dominios.some(function(elemento) {
+  return elemento.test(formato);
+});
+
+console.log(pruebaFormato);
+
+
+
+
+
+
+
+
+
