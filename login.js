2fea22f44511f986e65fcc59a088074e14d6ae23
Facu <facuovalles@gmail.com>
2023-06-20 17:08:43 -0300

    Message login js, css y html

--- login.js
+++ login.js
@@ -1,18 +1,21 @@
 usuarios = [
     {
-    name: 'facundo',
-    email: 'holamanola@gmail.com',
+    name: 'Facundo',
+    lastName: 'Ovalles',
+    email: 'admin@funkos.com',
     password: 'hola123',
+    userType: 'admin',
     }
 ]
 
 //**LOGIN**   
 //1 - Seleccionnar elementos
-var emailInput = document.getElementById('userEmailInput');
-var passwordInput = document.getElementById('userPasswordlInput');
-var loginButton = document.getElementById('loginButton');
-var remindMeInput = document.getElementById('remindMe');
-var forgotPassword = document.getElementById('forgotPassword');
+let userEmailForm = document.getElementById('userEmailForm');
+let emailInput = document.getElementById('userEmailInput');
+let passwordInput = document.getElementById('userPasswordlInput');
+let loginButton = document.getElementById('loginButton');
+let remindMeInput = document.getElementById('remindMe');
+let forgotPassword = document.getElementById('forgotPassword');
 
 //2 - Capturar info cuando se hace click
 loginButton.addEventListener('click', function() {
@@ -32,32 +35,68 @@ loginButton.addEventListener('click', function() {
 //usuario y password son correctos
 function validarData(email, password) {
   if (email === '') {
-    alert('Campo vacio. Por favor, ingrese email.');
+    var emailError = document.getElementById('emailError');
+    if (!emailError) {
+      //Creo el elemento que voy a agregar
+      var emailError = document.createElement('span');
+      //Le agrego un id para hacer la verificación
+      emailError.id = 'emailError';
+      //Le agrego una clase para darle formato
+      emailError.className = 'error-message';
+      //Al elemento que cree, le agrego el contenido
+      emailError.innerHTML = 'Campo vacío. Por favor, ingrese email.';
+      //Lo agrego en el elemento padre seleccionado
+      userEmailForm.parentNode.insertBefore(emailError, userEmailForm.nextSibling);
+    }
     return false;
-  }
-    
-  if (password === '') {
-    alert('Campo vacio. Por favor, ingrese password.');
+  }  
+  
+  else if (password === '') {
+    var passError = document.getElementById('passError');
+    if (!passError) {
+      var passError = document.createElement('span');
+      passError.id = 'passError';
+      passError.className = 'error-message';
+      passError.innerHTML = 'Campo vacío. Por favor, ingrese password.';
+      var userPassForm = document.getElementById('userPassForm');
+      userPassForm.parentNode.appendChild(passError);
+    }
     return false;
   }
 
-  if (email !== usuarios[0].email & password === usuarios[0].password || 
+  else if (email !== usuarios[0].email & password === usuarios[0].password || 
     email === usuarios[0].email & password !== usuarios[0].password ) {
     alert('Email o password inválidos.');
     return false;  
   }
   // Los campos son válidos
-  if (email === usuarios[0].email & password === usuarios[0].password ) {
+  else if (email === usuarios[0].email & password === usuarios[0].password ) {
      var usuarioValido = true;
      console.log(usuarioValido)
       if (usuarioValido) { 
-        window.location.href = "/pages/create.html";
+        window.location.href = "/pages/admin.html";
       }
     return true;  
-  }  
+  } 
 }
 
 //4- Boton de restablecer contraseña - hacer un prompt para que el usuario pueda cargar su email
 forgotPassword.addEventListener('click', () => {
   prompt('Ingresar correo registrado para recibir código de autenticación')
 })
+
+//Borrado de mensaje de error cuando se hace click nuevamente en el formulario
+//Este es del email
+userEmailForm.addEventListener('click', function() {
+  var emailError = document.getElementById('emailError');
+  if (emailError) {
+    emailError.remove();
+  }
+});
+//Este es del password
+userPassForm.addEventListener('click', function() {
+  var passError = document.getElementById('passError');
+  if (passError) {
+    passError.remove();
+  }
+});
