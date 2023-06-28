d9b3d251ebeefbde086d9c080f028a8f2935b9c6
Facu <facuovalles@gmail.com>
2023-06-28 10:56:35 -0300

    Nueva rama node y slider index

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Exmaple app listening at http://localhost:${port}`);
+});
+
+
+
+
+
